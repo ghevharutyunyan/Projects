82524e790b8fea438fdf6a50f5d72b063837c29f
ghevharutyunyan <ghevond.harutyunyan.2001@mail.ru>
2024-12-15 18:59:38 +0400

    Add files via upload

---
+++ Algorithms/Topological Sort with Algorithm Kahn.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+void Top_Sort(std::vector<std::vector<int>>& graph, int n) {
+    std::vector<int> inDegree(n, 0);
+    for (int i = 0; i < n; ++i) {
+        for (int v : graph[i]) {
+            inDegree[v]++;
+        }
+    }
+
+    std::queue<int> q;
+
+    for (int i = 0; i < inDegree.size(); ++i) {
+        if (inDegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    int count = 0;
+    while (!q.empty()) {
+        int vertex = q.front();
+        q.pop();
+
+        for (int u : graph[vertex]) {
+            inDegree[u]--;
+            if (inDegree[u] == 0) {
+                q.push(u);
+            }
+        }
+        count++;
+    }
+
+    if (count != n) {
+        std::cout << "Graph has a cycle!\n";
+    }
+    else {
+        std::cout << "Topological Sort successful.\n";
+    }
+}
+
+int main() {
+    
+    std::vector<std::vector<int>> graph = {
+        {1, 2},        
+        {3},           
+        {3},           
+        {},            
+        {5},           
+        {}             
+    };
+
+    int n = graph.size(); 
+
+    Top_Sort(graph, n);
+
+    return 0;
+}
+
+
---
+++ Algorithms/Topological Sort with DFS.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+void dfs(int u, std::vector<std::vector<int>>& graph, std::vector<bool> visited, std::stack<int>& topoStack) {
+    visited[u] = true;
+
+    for (int neighbor : graph[u]) {
+        if (!visited[neighbor]) {
+            dfs(neighbor, graph, visited, topoStack);
+        }
+    }
+
+    topoStack.push(u);
+}
+
+std::vector<int> topSort(int n, std::vector<std::vector<int>>& graph) {
+    std::vector<bool> visited(n, false);
+    std::stack<int> st;
+    
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i]) {
+            dfs(i, graph, visited, st);
+        }
+    }
+
+    std::vector<int> topOrder;
+
+    while (!st.empty()) {
+        topOrder.push_back(st.top());
+        st.pop();
+    }
+    return topOrder;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
